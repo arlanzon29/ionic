e8e6bebf84479e0e571bb56e2507194209d7f4f3
Daniel Rodríguez <daniel.rodriguez@gisaconsultores.dominio>
2016-03-11 17:13:27 +0100

    Prueba 2

--- app/pages/interlocutores/interlocutores.js
+++ app/pages/interlocutores/interlocutores.js
@@ -2,8 +2,6 @@ import {Page, NavController, NavParams} from 'ionic-angular';
 import {Inject} from 'angular2/core';
 import {InterlocutorMainPage} from '../interlocutor-main/interlocutor-main';
 
-
-
 @Page({
   templateUrl: 'build/pages/interlocutores/interlocutores.html'
 })
@@ -15,14 +13,21 @@ export class InterlocutoresPage {
  constructor(nav, navParams) {
     this.nav = nav;
 
-   	this.interlocutores=[{'CardCode':'C001','CardName':'Casple','LicTradNum':'13000000','CardFName':'Casple',
-    'OCPR':[{'Name':'Jose Manuel','FirstName':'Jose Manuel','LastName':'','Tel1':'947483890','Tel2':'','Cellolar':'','EMail':'casple@capsle.es'},
-          {'Name':'Rosa','FirstName':'Rosa','LastName':'','Tel1':'947483890','Tel2':'','Cellolar':'','EMail':''}],
-    'CRD1':[{'Adress':'Dir. Facturacion','Street':'Calle Alcalde','Block':'Sin numero','City':'Burgos','ZipCode':'09007','County':'Burgos','AdresType':'B'},
-    {'Adress':'Dir. Envio','Street':'Calle Alcalde','Block':'Sin numero','City':'Burgos','ZipCode':'09007','County':'Burgos','AdresType':'S'}]},
-   						{'CardCode':'C002','CardName':'Ima1','LicTradNum':'13000001','CardFName':'Ima1','OCPR':[],'CRD1':[]},
-   						{'CardCode':'C003','CardName':'Greenland','LicTradNum':'13000002','CardFName':'Greenland','OCPR':[],'CRD1':[]}
-   						]
+    var inter=window.localStorage.getItem("interlocutores")
+
+    if (!inter){
+           	this.interlocutores=[{'CardCode':'C001','CardName':'Casple','LicTradNum':'13000000','CardFName':'Casple',
+            'OCPR':[{'Name':'Jose Manuel','FirstName':'Jose Manuel','LastName':'','Tel1':'947483890','Tel2':'','Cellolar':'','EMail':'casple@capsle.es'},
+                  {'Name':'Rosa','FirstName':'Rosa','LastName':'','Tel1':'947483890','Tel2':'','Cellolar':'','EMail':''}],
+            'CRD1':[{'Adress':'Dir. Facturacion','Street':'Calle Alcalde','Block':'Sin numero','City':'Burgos','ZipCode':'09007','County':'Burgos','AdresType':'B'},
+            {'Adress':'Dir. Envio','Street':'Calle Alcalde','Block':'Sin numero','City':'Burgos','ZipCode':'09007','County':'Burgos','AdresType':'S'}]},
+           						{'CardCode':'C002','CardName':'Ima1','LicTradNum':'13000001','CardFName':'Ima1','OCPR':[],'CRD1':[]},
+           						{'CardCode':'C003','CardName':'Greenland','LicTradNum':'13000002','CardFName':'Greenland','OCPR':[],'CRD1':[]}
+           						]
+             window.localStorage.setItem("interlocutores",JSON.stringify(this.interlocutores));                      
+    }else{
+      this.interlocutores=JSON.parse(inter);
+    }
   }
 
  itemTapped(event, interlocutor) {
