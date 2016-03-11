14820e45bf88914bc67086453d21e97d383d4f65
Daniel Rodríguez <daniel.rodriguez@gisaconsultores.dominio>
2016-03-11 17:44:28 +0100

    Meto la configuracion

--- app/pages/interlocutores/interlocutores.js
+++ app/pages/interlocutores/interlocutores.js
@@ -5,7 +5,7 @@ import {InterlocutorMainPage} from '../interlocutor-main/interlocutor-main';
 @Page({
   templateUrl: 'build/pages/interlocutores/interlocutores.html'
 })
-export class InterlocutoresPage {
+export class InterlocutoresPage  {
    static get parameters() {
     return [[NavController], [NavParams]];
   }
@@ -16,18 +16,12 @@ export class InterlocutoresPage {
     var inter=window.localStorage.getItem("interlocutores")
 
     if (!inter){
-           	this.interlocutores=[{'CardCode':'C001','CardName':'Casple','LicTradNum':'13000000','CardFName':'Casple',
-            'OCPR':[{'Name':'Jose Manuel','FirstName':'Jose Manuel','LastName':'','Tel1':'947483890','Tel2':'','Cellolar':'','EMail':'casple@capsle.es'},
-                  {'Name':'Rosa','FirstName':'Rosa','LastName':'','Tel1':'947483890','Tel2':'','Cellolar':'','EMail':''}],
-            'CRD1':[{'Adress':'Dir. Facturacion','Street':'Calle Alcalde','Block':'Sin numero','City':'Burgos','ZipCode':'09007','County':'Burgos','AdresType':'B'},
-            {'Adress':'Dir. Envio','Street':'Calle Alcalde','Block':'Sin numero','City':'Burgos','ZipCode':'09007','County':'Burgos','AdresType':'S'}]},
-           						{'CardCode':'C002','CardName':'Ima1','LicTradNum':'13000001','CardFName':'Ima1','OCPR':[],'CRD1':[]},
-           						{'CardCode':'C003','CardName':'Greenland','LicTradNum':'13000002','CardFName':'Greenland','OCPR':[],'CRD1':[]}
-           						]
-             window.localStorage.setItem("interlocutores",JSON.stringify(this.interlocutores));                      
+           	alert("Vaya a configuracion e inice las tablas")
+                                 
     }else{
       this.interlocutores=JSON.parse(inter);
     }
+
   }
 
  itemTapped(event, interlocutor) {
@@ -35,4 +29,10 @@ export class InterlocutoresPage {
        interlocutor: interlocutor
      });
   }
+
+  save(){
+    window.localStorage.setItem("interlocutores",JSON.stringify(this.interlocutores));  
+    alert("Interlocutores Guardados")
+  }
+   
 }
